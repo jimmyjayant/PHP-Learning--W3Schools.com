0340cf9b9a447caa22c78653b3f03d10474af580
jimmyjayant <jayantjimmy49@gmail.com>
2023-04-20 20:00:02 +0530

    W3Schools.com PHP Tutorials upto tutorial 38 PHP Exceptions

---
+++ 37. PHP JSON-Looping Through Values 1.php
@@ -0,0 +1,9 @@
+<?php
+$jsonobj = '{"Peter":35,"Ben":37,"Joe":43}';
+
+$arr = json_decode($jsonobj, true);
+
+foreach($arr as $key => $value) {
+    echo $key . "=> " . $value . "<br>";
+}
+?>
---
+++ 37. PHP JSON-Looping Through Values.php
@@ -0,0 +1,9 @@
+<?php
+$jsonobj = '{"Peter":35,"Ben":37,"Joe":43}';
+
+$obj = json_decode($jsonobj);
+
+foreach($obj as $key => $value) {
+    echo $key . "=> " . $value . "<br>";
+}
+?>
