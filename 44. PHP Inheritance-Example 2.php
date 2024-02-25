000e9f630c7dfe56c0b336c3a9c041dd6b83ed80
JIMMY JAYANT <113120690+jimmyjayant@users.noreply.github.com>
2024-02-25 18:01:45 +0530

    Make some changes to PHP Inheritance Example 2 and 5.php

--- 44. PHP Inheritance-Example 2.php
+++ 44. PHP Inheritance-Example 2.php
@@ -1,6 +1,14 @@
 <?php
 namespace Example2;
 
+//Import the Exception class
+use Exception; 
+
+// Custom error handler function
+function customErrorHandler($exception) {
+  echo "<p style='color:red'>Custom Error:- " . $exception->getMessage() . "</p>"; 
+}
+
 class Fruit {
     public $name;
     public $color;
@@ -11,17 +19,28 @@ class Fruit {
 
     protected function intro() {
         echo "The fruit is {$this->name} and the color is {$this->color}.";
+        throw new Exception("Something went wrong!");
     }
 }
 
 class Strawberry extends Fruit {
     public function message() {
-        echo "Am I a fruit or a berry? ";
+        echo "Am I a fruit or a berry?<br> ";
     }
 }
 
+// Set the custom error handler
+set_exception_handler('Example2\customErrorHandler');
+
 // Try to call all three methods from outside class
 $strawberry = new Strawberry("Strawberry", "red"); // OK. __construct() is public
+
 $strawberry->message(); // OK. message() is public
-$strawberry->intro(); // ERROR. intro() is protected 
+
+try {
+    $strawberry->intro(); // ERROR. intro() is protected 
+}
+catch(Exception $e) {
+    //Exception will be caught by the custom error handler
+}
 ?>
--- 44. PHP Inheritance-Example 5.php
+++ 44. PHP Inheritance-Example 5.php
@@ -1,11 +1,32 @@
 <?php
 namespace Example5;
-final class Fruit {
-    // some code
+
+
+//Import the Exception class
+use Exception; 
+
+// Custom error handler function
+function customErrorHandler($exception) {
+  echo "<p style='color:red'>Custom Error:- " . $exception->getMessage() . "</p>"; 
 }
 
-// will result in error 
-class Strawberry extends Fruit {
+final class Fruit {
     // some code
+    public function err() {
+      throw new Exception("ERROR");
+    }
+}
+
+// Set the custom error handler
+set_exception_handler('Example5\customErrorHandler');
+
+// will result in error
+try { 
+    class Strawberry extends Fruit {
+        // some code
+    }
+}
+catch(Exception $e) {
+    //Exception will be caught by the custom error handler
 }
 ?>
